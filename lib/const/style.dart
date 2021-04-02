10dbb0e96178da2b05d57cb0207b075c7d9a8272
saulzh <z@ruiruizhang.com>
2021-04-02 08:55:23 +0800

    update

---
+++ lib/const/style.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Style {
+  static TextStyle white = TextStyle(color: Colors.white);
+}
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_collection/const/style.dart';
 
 class Home extends StatefulWidget {
   _HomeState createState() => _HomeState();
@@ -14,10 +15,40 @@ class _HomeState extends State<Home> {
     var home = MaterialApp(
       home: Scaffold(
         body: Container(
-          child: Text('home'),
-        ),
+            child: Column(
+          children: [
+            header(context),
+          ],
+        )),
       ),
     );
     return home;
+  }
+
+  Widget header(BuildContext context) {
+    return Container(
+      height: 50,
+      decoration: BoxDecoration(
+          border: Border.all(
+        color: Colors.grey,
+      )),
+      child: Row(
+        children: [
+          Container(
+            margin: EdgeInsets.only(
+              left: 10,
+              right: 10,
+            ),
+            child: Icon(
+              Icons.thumb_up,
+              color: Colors.redAccent,
+            ),
+          ),
+          Container(
+            child: Text('FLutter web test'),
+          ),
+        ],
+      ),
+    );
   }
 }
